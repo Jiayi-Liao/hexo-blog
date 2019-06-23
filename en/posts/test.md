2f479382c2414411610d4c848ea3c133f742efd5
Jiayi Liao <bupt_ljy@163.com>
2019-06-23 20:12:23 +0800

    first deploy

---
+++ en/posts/test.md
@@ -0,0 +1,10 @@
+---
+title: english-test
+date: 2019-06-23 17:13:33
+tags:
+keywords:
+description:
+categories:
+---
+
+This is a testsssssss!
---
+++ tasks.py
@@ -0,0 +1,43 @@
+# coding=utf8
+from fabric import Connection
+from invoke import task
+from password import *
+
+
+@task
+def blogpic(c, src):
+    conn = Connection(host='101.200.171.13', user='root',
+                      connect_kwargs={"password": password})
+    src = src.split("/")[-1]
+    conn.put("/Users/liaojiayi/Desktop/" + src,
+             "/server/hexo/themes/next/source/assets/")
+    conn.run("cp /server/hexo/themes/next/source/assets/" +
+             src + " /var/www/hexo/assets/")
+
+
+@task
+def upload(c, src, lang):
+    if lang == "en":
+        dest = "/server/hexoen/source/_posts/"
+    elif lange == "ch":
+        dest = "/server/hexo/source/_posts/"
+    else:
+        exit(100)
+    conn = Connection(host='101.200.171.13', user='root',
+                      connect_kwargs={"password": password})
+    conn.put(src, dest)
+
+
+@task
+def deployEn(c):
+    conn = Connection(host='101.200.171.13', user='root',
+                      connect_kwargs={"password": password})
+    conn.run("cd /server/hexoen/ && /server/node-v6.11.2/bin/hexo g")
+    conn.run("cp -r /server/hexoen/public/* /server/hexo/public/en/")
+
+
+@task
+def deploy(c):
+    conn = Connection(host='101.200.171.13', user='root',
+                      connect_kwargs={"password": password})
+    conn.run("cd /server/hexo/ && /server/node-v6.11.2/bin/hexo g -d")
